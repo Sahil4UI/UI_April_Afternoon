f8b9837352bc69725bc420ab3f640e9f77ff7ad4
Sahil4UI <54834362+Sahil4UI@users.noreply.github.com>
2020-05-30 17:04:28 +0530

    Add files via upload

---
+++ JS/JSMUSICAPP/Assets/JS/songsList.js
@@ -0,0 +1,31 @@
+var songsList = [
+
+{
+    "songName": "Tibbeyan Da Putt",
+    "artistname":"Siddu Moosewala",
+    "songSrc":"Assets/songs/Sidhu Moosewala Tibbeyan Da Putt Song Download(DjBharat.IN).mp3",
+    "songImage":"https://1.bp.blogspot.com/-ctGwwn4SrOY/Xn-Z0SQw6EI/AAAAAAAACd0/yERUsvI3Okwol2TnwDiITT0TF-fE8oMKQCNcBGAsYHQ/s1600/bhy.jpg";
+}
+,
+
+{
+    "songName": "Tibbeyan Da Putt",
+    "artistname":"Siddu Moosewala",
+    "songSrc":"Assets/songs/Sidhu Moosewala Tibbeyan Da Putt Song Download(DjBharat.IN).mp3",
+    "songImage":"https://1.bp.blogspot.com/-ctGwwn4SrOY/Xn-Z0SQw6EI/AAAAAAAACd0/yERUsvI3Okwol2TnwDiITT0TF-fE8oMKQCNcBGAsYHQ/s1600/bhy.jpg";
+},
+
+{
+    "songName": "Tibbeyan Da Putt",
+    "artistname":"Siddu Moosewala",
+    "songSrc":"Assets/songs/Sidhu Moosewala Tibbeyan Da Putt Song Download(DjBharat.IN).mp3",
+    "songImage":"https://1.bp.blogspot.com/-ctGwwn4SrOY/Xn-Z0SQw6EI/AAAAAAAACd0/yERUsvI3Okwol2TnwDiITT0TF-fE8oMKQCNcBGAsYHQ/s1600/bhy.jpg";
+},
+{
+    "songName": "Tibbeyan Da Putt",
+    "artistname":"Siddu Moosewala",
+    "songSrc":"Assets/songs/Sidhu Moosewala Tibbeyan Da Putt Song Download(DjBharat.IN).mp3",
+    "songImage":"https://1.bp.blogspot.com/-ctGwwn4SrOY/Xn-Z0SQw6EI/AAAAAAAACd0/yERUsvI3Okwol2TnwDiITT0TF-fE8oMKQCNcBGAsYHQ/s1600/bhy.jpg";
+}
+
+]
